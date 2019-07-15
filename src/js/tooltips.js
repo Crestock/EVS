74556402001315749f3023ca54a39e608ee7f6ec
Crestock <47856835+Crestock@users.noreply.github.com>
2019-07-15 10:14:45 -0700

    update with files

---
+++ src/js/tooltips.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+ * --------------------------------------------------------------------------
+ * CoreUI Free Boostrap Admin Template (v2.1.14): tooltips.js
+ * Licensed under MIT (https://envisionsolar.com/license)
+ * --------------------------------------------------------------------------
+ */
+$('[data-toggle="tooltip"]').tooltip();
+//# sourceMappingURL=tooltips.js.map
